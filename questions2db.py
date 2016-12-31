7659b623b0e5850aeebd3530d4319bac65d525fd
Alexander <alexbrianzhang@gmail.com>
2016-12-30 19:00:48 -0500

    Redid entire question database with better formatting and math & compsci questions

--- questions2db.py
+++ questions2db.py
@@ -3,63 +3,75 @@ from app import app, db
 from app.models import Question
 import os, re, json, unicodedata
 
-rounds_path = 'official/text' # A folder of OCR'd Science Bowl sample rounds
-questions = []
-regex = re.compile(r'TOSS-UP\s*'  # type of question
-                    r'(\d+)(?:\)|\.)\s*'   # question number
-                    r'(PHYSICS|GENERAL SCIENCE|ENERGY|EARTH AND SPACE|EARTH SCIENCE|CHEMISTRY|BIOLOGY|ASTRONOMY|MATH|COMPUTER SCIENCE) '   # category of question, no MATH because OCR problems
-                    #r'(ASTR|BIOL|CHEM|ERSC|GENR|PHYS); '
-                    r'(Short Answer|Multiple Choice) '  # format of question
-                    #r'(Short Answer|Multiple Choice): '
-                    r'([\s\S]+?(?=ANSWER))'  # actual question text
-                    r'ANSWER: ([\s\S]+?(?=BONUS))'
-                    r'\s*BONUS\s*'  # type of question
-                    r'(\d+)(?:\)|\.)\s'   # question number
-                    r'(PHYSICS|GENERAL SCIENCE|ENERGY|EARTH AND SPACE|EARTH SCIENCE|CHEMISTRY|BIOLOGY|ASTRONOMY|MATH|COMPUTER SCIENCE) '   # category of question, no MATH because OCR problems
-                    #r'(ASTR|BIOL|CHEM|ERSC|GENR|PHYS); '
-                    r'(Short Answer|Multiple Choice) '  # format of question
-                    #r'(Short Answer|Multiple Choice): '
-                    r'([\s\S]+?(?=ANSWER))'  # actual question text
-                    r'ANSWER: ([\s\S]+?(?=\n\s|\n\d{4}|High|Round|ROUND|TOSS-UP|BONUS|$))', re.I)   # answer to question
+source_name = 'CSUB' # Use Official, 98Nats, 05Nats, or CSUB (folder names)
+
+rounds_path = source_name+'/text' # A folder of OCR'd Science Bowl sample rounds
+if source_name == '98Nats':
+    regex = re.compile(r'TOSS-UP\s' 
+                        r'(\d+)(?:\)|\.) ' # question number
+                        r'(ASTR|BIOL|CHEM|ERSC|GENR|PHYS|MATH|COMP); ' # category of question
+                        r'(Short Answer|Multiple Choice): ' # format of toss up
+                        r'([\s\S]+?(?=ANSWER))' # toss up question text
+                        r'ANSWER: ([\s\S]+?(?=BONUS))' # toss up answer
+                        r'\s*BONUS\s'
+                        r'(\d+)(?:\)|\.) '
+                        r'(ASTR|BIOL|CHEM|ERSC|GENR|PHYS|MATH|COMP); '
+                        r'(Short Answer|Multiple Choice): ' # format of bonus
+                        r'([\s\S]+?(?=ANSWER))' #  bonus question text
+                        r'ANSWER: ([\s\S]+?(?=TOSS-UP|ROUND|\Z))', re.M) # bonus answer
+else:
+    regex = re.compile(r'TOSS-UP\s'
+                        r'(\d+)(?:\)|\.) ' # question number
+                        r'(PHYSICS|GENERAL SCIENCE|ENERGY|EARTH AND SPACE|EARTH SCIENCE|CHEMISTRY|BIOLOGY|ASTRONOMY|MATH|COMPUTER SCIENCE) ' # category of question
+                        r'(Short Answer|Multiple Choice) ' # format of toss up
+                        r'([\s\S]+?(?=ANSWER))' # toss up question text
+                        r'ANSWER: ([\s\S]+?(?=BONUS))' # toss up answer
+                        r'\s*BONUS\s'
+                        r'(\d+)(?:\)|\.) ' 
+                        r'(PHYSICS|GENERAL SCIENCE|ENERGY|EARTH AND SPACE|EARTH SCIENCE|CHEMISTRY|BIOLOGY|ASTRONOMY|MATH|COMPUTER SCIENCE) '
+                        r'(Short Answer|Multiple Choice) ' # format of bonus
+                        r'([\s\S]+?(?=ANSWER))' # bonus question text
+                        r'ANSWER: ([\s\S]+?(?=TOSS-UP|ROUND|\Z))', re.M) # bonus answer
 
 for filename in os.listdir(rounds_path):
     if filename.endswith('.txt'):
         with open(os.path.join(rounds_path, filename), encoding='utf8') as file:
-            #source = 'CSUB-' + filename[1:-4]
-            source = 'official-' + filename[:-4]
-            #source = '05Nats-' + filename[:-4]
-            #source = '98Nats-' + filename[:-4]
-            for match in regex.findall(file.read()):
-                # category = 'ERROR'
-                # if match[1] == 'ASTR':
-                #     category = 'ASTRONOMY'
-                # elif match[1] == 'BIOL':
-                #     category = 'BIOLOGY'
-                # elif match[1] == 'CHEM':
-                #     category = 'CHEMISTRY'
-                # elif match[1] == 'ERSC':
-                #     category = 'EARTH SCIENCE'
-                # elif match[1] == 'GENR':
-                #     category = 'GENERAL SCIENCE'
-                # elif match[1] == 'PHYS':
-                #     category = 'PHYSICS'
-                # else:
-                #     print('No proper category on ' + source + ', question ' + match[0])
 
+            source = source_name+'-'+filename[:-4]
+
+            for match in regex.findall(file.read()):
+                if source_name == '98Nats':
+                    if match[1] == 'ASTR':
+                        category = 'ASTRONOMY'
+                    elif match[1] == 'BIOL':
+                        category = 'BIOLOGY'
+                    elif match[1] == 'CHEM':
+                        category = 'CHEMISTRY'
+                    elif match[1] == 'ERSC':
+                        category = 'EARTH SCIENCE'
+                    elif match[1] == 'GENR':
+                        category = 'GENERAL SCIENCE'
+                    elif match[1] == 'PHYS':
+                        category = 'PHYSICS'
+                    elif match[1] == 'MATH':
+                        category = 'MATH'
+                    elif match[1] == 'COMP':
+                        category = 'COMPUTER SCIENCE'
+                    else:
+                        print('No proper category on ' + source + ', question ' + match[0])
+                else:
+                    category = match[1]
+
+                match = [item.rstrip() for item in match] # strip trailing whitespace from all fields
                 db.session.add(Question(
-                    #category=category,
-                    category=match[1],
+                    category=category,
                     source=source,
                     tossup_format=match[2],
                     tossup_question=re.sub(r'\n\s+', '\n', match[3]),
                     tossup_answer=match[4],
-                    #tossup_answer=unicodedata.normalize('NFKD', re.sub(r'\n', ' ', match[4])).encode('ASCII', 'ignore').decode('utf8'),   # replace letters that cannot be typed on keyboard
                     bonus_format=match[7],
                     bonus_question=re.sub(r'\n\s+', '\n', match[8]),
                     bonus_answer=match[9]
-                    #bonus_answer=unicodedata.normalize('NFKD', re.sub(r'\n', ' ', match[9])).encode('ASCII', 'ignore').decode('utf8')   # replace letters that cannot be typed on keyboard
                 ))
 
-
-#json.dump(questions, open('questions.json', 'a+'), indent=4, separators=(',', ': '), sort_keys=True)  # make JSON pretty looking
 db.session.commit()
