2d50c7d54a0700844e10b47c8a9daa2ac441b137
Alexander <alexbrianzhang@gmail.com>
2017-01-30 09:58:10 -0500

    Added full text search, report function, tweaked logo and about

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
-from app import db
+from app import db, app
+from sqlalchemy.sql import expression
+import flask_whooshalchemyplus as whooshalchemy
 
 class Question(db.Model):
+    __searchable__ = ['tossup_question', 'tossup_answer', 'bonus_question', 'bonus_answer']
+
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.Text, index=True)
     source = db.Column(db.Text, index=True)
@@ -10,9 +14,12 @@ class Question(db.Model):
     bonus_format = db.Column(db.Text, index=True)
     bonus_question = db.Column(db.Text)
     bonus_answer = db.Column(db.Text)
+    reported = db.Column(db.Boolean, server_default=expression.literal(False), default=False)
 
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     def __repr__(self):
         return '<Question {}, {}>'.format(self.id, self.source)
+
+whooshalchemy.whoosh_index(app, Question)
--- app/views.py
+++ app/views.py
@@ -2,9 +2,8 @@ from flask import abort, jsonify, make_response, request, url_for, render_templa
 from app import app, db
 from config import QUESTIONS_PER_PAGE
 from sqlalchemy import or_
-import re
+import re, random
 from .models import Question
-import random
 
 @app.route('/api/v1.0/questions/', methods=['GET'])
 def get_questions():
@@ -13,40 +12,28 @@ def get_questions():
 
 @app.route('/api/v1.0/questions/random', methods=['GET'])
 def get_random_question():
-    questions = Question.query.all()
-    return jsonify({'question': random.choice([make_public(q) for q in questions])})
+    question = random.choice(Question.query.all())
+    return jsonify({'question': make_public(question)})
 
 @app.route('/api/v1.0/questions/<int:question_id>', methods=['GET'])
 def get_question(question_id):
-    q = Question.query.get_or_404(question_id)
-    return jsonify({'question': make_public(q)})
+    question = Question.query.get_or_404(question_id)
+    return jsonify({'question': make_public(question)})
 
 # Filter for questions with certain attributes using post requests
 @app.route('/api/v1.0/questions/', methods=['POST'])
 def get_questions_filtered():
     if not request.json:
         abort(400)
-    # filter questions by category and source
-    if 'categories' in request.json and len(request.json['categories']) > 0:
-        questions = list(Question.query.filter(Question.category.in_(request.json['categories'])))
-    else:
-        questions = list(Question.query.all())
-    if 'sources' in request.json and len(request.json['sources']) > 0:
-        questions = [q for q in questions if q.source.startswith(tuple(request.json['sources']))]
+    questions = filter(request.json).all()
     return jsonify({'questions': [make_public(q) for q in questions]})
 
 @app.route('/api/v1.0/questions/random', methods=['POST'])
 def get_random_question_filtered():
     if not request.json:
         abort(400)
-    # filter questions by category and source
-    if 'categories' in request.json and len(request.json['categories']) > 0:
-        questions = list(Question.query.filter(Question.category.in_(request.json['categories'])))
-    else:
-        questions = list(Question.query.all())
-    if 'sources' in request.json and len(request.json['sources']) > 0:
-        questions = [q for q in questions if q.source.startswith(tuple(request.json['sources']))]
-    return jsonify({'question': random.choice([make_public(q) for q in questions])})
+    question = random.choice(filter(request.json).all())
+    return jsonify({'question': make_public(question)})
 
 def make_public(question, html=False):
     new_question = {}
@@ -67,17 +54,11 @@ def not_found(error):
 @app.route('/')
 @app.route('/tossup')
 def tossup():
-    # filter questions by category and source
-    if 'categories' in session and len(session['categories']) > 0:
-        questions = list(Question.query.filter(Question.category.in_(session['categories'])))
-    else:
-        questions = list(Question.query.all())
-    if 'sources' in session and len(session['sources']) > 0:
-        questions = [q for q in questions if q.source.startswith(tuple(session['sources']))]
+    questions = filter().all()
     # reset settings if they filter out all questions, for example if the source does not contain any questions of a particular category
-    if len(questions) == 0:
+    if len(questions) <= 0:
         flash("The inputted settings did not match any available questions")
-        questions=list(Question.query.all())
+        questions=Question.query.all()
         session['categories'] = []
         session['sources'] =[]
     question = random.choice(questions)
@@ -89,37 +70,68 @@ def bonus():
     if 'question_id' in session:
         question = Question.query.get(session['question_id'])
     else:
-       question = random.choice(list(Question.query.all()))
+        questions = filter().all()
+        if len(questions) <= 0:
+            flash("The inputted settings did not match any available questions")
+            questions=Question.query.all()
+            session['categories'] = []
+            session['sources'] = []
+        question = random.choice(questions)
     return render_template('bonus.html', question=make_public(question, html=True), settings=session)
 
 @app.route('/browse')
 @app.route('/browse/<int:page>')
 def browse(page=1):
-    # filter questions by category and source
-    if 'categories' in session and len(session['categories']) > 0:
-        questions = Question.query.filter(Question.category.in_(session['categories']))
-    else:
-        questions = Question.query;
-    if 'sources' in session and len(session['sources']) > 0:
-        questions = questions.filter(or_(*[Question.source.startswith(source) for source in session['sources']]))
-    # reset settings if they filter out all questions, for example if the source does not contain any questions of a particular category
+    questions = filter()
+    if 'search' in session and session['search'] is not None and len(session['search']) > 0:
+        questions = questions.whoosh_search(session['search'])
     questions = questions.paginate(page, QUESTIONS_PER_PAGE, False)
-    if len(questions.items) == 0:
+    if len(questions.items) <= 0:
         flash("The inputted settings did not match any available questions")
-        questions=Question.query.paginate(page, QUESTIONS_PER_PAGE, False);
+        questions=Question.query.paginate(page, QUESTIONS_PER_PAGE, False)
         session['categories'] = []
         session['sources'] = []
+        session['search'] = ''
     return render_template('browse.html', questions=[make_public(question, html=True) for question in questions.items], settings=session, pagination=questions)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', settings=session, num_questions=len(Question.query.all())*2)
+    return render_template('about.html', settings=session, num_questions=len(Question.query.all())*2) # counting both tossup and bonuses
 
 @app.route('/settings', methods=['POST'])
 def settings():
     session['categories'] = request.form.getlist('category')
     session['sources'] = request.form.getlist('source')
     return redirect(request.args.get('next') or url_for('tossup'))
+
+@app.route('/search', methods=['POST'])
+def search():
+    session['search'] = request.form.get('search')
+    return redirect(url_for('browse'))
+
+def filter(params=session):
+    # filter questions by category and source
+    questions = Question.query
+    if 'categories' in params and params['categories'] is not None and len(params['categories']) > 0:
+        questions = questions.filter(Question.category.in_(params['categories']))
+    if 'sources' in params and params['sources'] is not None and len(params['sources']) > 0:
+        questions = questions.filter(or_(*[Question.source.startswith(source) for source in params['sources']]))
+    return questions
+
+@app.route('/report', methods=['POST'])
+def report():
+    question = Question.query.get(request.form.get('uri')[len(url_for('get_questions', _external=True)):])
+    print(request.form.get('uri')[len(url_for('get_questions', _external=True)):])
+    question.reported = True
+    db.session.commit()
+    flash("Thank you for reporting a question in need of improvement!")
+    return redirect(request.args.get('next') or url_for('tossup'))
+
+@app.route('/viewreports')
+@app.route('/viewreports/<int:page>')
+def viewreports(page=1):
+    questions = Question.query.filter(Question.reported == True).all()
+    return jsonify({'questions': [make_public(q) for q in questions]})
 
 # set the secret key. keep this really secret (put somewhere more private in the future)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
